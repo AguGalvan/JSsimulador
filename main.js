835e6df2d8b774886b46acfb8889e6640c766668
AguGalvan <agus.oktubre@gmail.com>
2022-10-29 23:14:49 -0300

    primer commit

--- main.js
+++ main.js
@@ -1,133 +1,63 @@
-// ESTRUCTURA IF
+function comprarProductos() {
+    let producto = '';
+    let precio = 0;
+    let cantidad = 0;
+    let totalCompra = 0;
+    let cantidadTotal = 0;
+    let seguirComprando = false;
 
-//true // Es un valor booleano que es evaluado como verdadero
-//false // Es un valor booleano que es evaluado como falso
+    do {
+        producto = prompt('¿Queres comprar helado de chocolate, vainilla o ambos?', 'Ej: ambos');
+        cantidad = parseInt(prompt('¿Cuantos queres comprar?'));
 
-// if(true) {
-//     console.log("Este mensaje se va a mostrar")
-// }
+        let cantidadValidada = validarCantidad(cantidad);
 
-// if(false) {
-//     console.log("Este mensaje se va a mostrar")
-// }
+        switch (producto) {
+            case 'chocolate':
+                precio = 700;
+                break;
+            case 'vainilla':
+                precio = 700;
+                break;
+            case 'ambos':
+                precio = 1400;
+                break;
+            default:
+                alert('Alguno de los datos ingresados no es correcto.');
+                precio = 0;
+                cantidad = 0;
+                break;
+        }
 
+        totalCompra += precio * cantidadValidada;
+        seguirComprando = confirm('¿Queres agregar otro producto?');
 
-
-// Operador de asignacion =
-// Operador de comparacion ==
-
-// let numero = 4
-
-// if(numero == 2) {
-//     console.log("Es verdadero")
-// }
-
-
-
-// let stock = 0
-
-// if (stock == 0) {
-//     alert("Necesitamos stock de este producto")
-// }
-
-
-// Operadores de igualdad debil== (loose equality)
-
-// Coercion de tipos
-
-
-// if (2 == "2"){
-//     console.log("Esto es correcto")
-// }
-
-// Operador de igualdad estricta === (strict equality)
-
-// if (2 === "2"){
-//     console.log("Esto es correcto")
-// } 
+    } while (seguirComprando)
 
 
-// Ejemplo if else
+    return totalCompra;
+}
 
-// let nombreDeUsuario = prompt("Ingrese su nombre de usuario:");
+function validarCantidad(cantidad) {
+    while(Number.isNaN(cantidad) || cantidad === 0) {
+        if (cantidad !== 0) {
+            alert('Debe agregar un número.')
+        } else {
+            alert('Debe ingresar un número distinto de cero.')
+        }
+        cantidad = parseInt(prompt('¿Cuantos queres comprar?'));
+    }
 
-// if (nombreDeUsuario === " "){
-//     alert("No ingresaste el nombre de usuario")
-// } else{
-//     alert("Nombre de usuario ingresado: "+nombreDeUsuario);
-// } 
+    return cantidad;
+}
 
+const totalCompra = comprarProductos();
 
-// Ejemplo de if else if
+function calcularTotalAPagar(totalCompra) {
+    totalCompra = totalCompra;
+    alert('El total a pagar es: $'+totalCompra);
+}
 
-// let precio = 50
-
-// if (precio < 20) {
-//     alert("el precio es menor a 20")
-// } else if (precio < 50) {
-//     alert("El precio es menor a 50")
-// } else if (precio < 100) {
-//     alert("El precio es menor a 100")
-// } else {
-//     alert("El precio es mayor a 100")
-// } 
-
-
-// TRUE or FALSE
-
-// let numero = 10 
-// let esMayorQue5 = (numero > 5)
-
-// if (esMayorQue5) {
-//     alert("Efectivamente "+ numero + " es mayor que 5")
-// } else {
-//     alert(numero+" No es mayor que 5")
-// }
-
-
-// AND
-
-// let nombreIngresado = prompt("Ingrese su nombre:", "Escriba acá");
-// let apellidoIngresado = prompt("Ingrese su nombre:", "Escriba acá");
-
-// if ( (nombreIngresado != "") && (apellidoIngresado != "") ) {
-//     alert ("Nombre: "+nombreIngresado+" Apellido: " +apellidoIngresado);
-// } else {
-//     alert ("Falta alguno de los datos")
-// }
-
-
-// OR 
-
-// let nombreIngresado = prompt("Ingrese su nombre");
-
-// if ( (nombreIngresado == "ANA") || (nombreIngresado == "ana") || (nombreIngresado == "Ana") ) {
-//     alert("El nombre ingresado es Ana")
-// } else {
-//     alert("El nombre ingresado NO es Ana")
-// }
-
-
-// Ejercicio de la clase
-
-// let numeroIngresado = prompt("Ingrese un numero");
-// let nombreIngresado = prompt("Ingrese texto");
-
-// if  ((numeroIngresado >= 1000)) {
-//     alert("El numero ingresado es mayor a 1000")
-// } else {
-//     alert("El numero ingresado es menor a 1000")
-// }
-
-// if ((nombreIngresado === "Hola")) {
-//     console.log("Hola!")
-// }
-
-// if ((numeroIngresado > 10) && (numeroIngresado < 50)) {
-//     alert("El valor es correcto");
-// } else {
-//     alert("El numero ingresado es incorrecto")
-// }
-
+calcularTotalAPagar(totalCompra);
 
 
